e7523f80a5205aae3d9cceff6500d942366c11ad
shw3 <shw3@njit.edu>
2024-02-29 13:38:17 -0500

    attaching rabbitXdatabase comms

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -1,7 +1,10 @@
 #!/usr/bin/php
 <?php
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
-$mydb = new mysqli('10.244.213.77','testUser','12345','testdb');
+#establishing databse connection with mySQL
+$mydb = new mysqli('127.0.0.1','testUser','12345','testdb');
 
 if ($mydb->errno != 0)
 {
@@ -21,5 +24,20 @@ if ($mydb->errno != 0)
         exit(0);
 }
 
+#establishing connection to rabbitMQ
 
+
+$connection = new AMQPStreamConnection('rabbitmq_host', 5672, 'user', 'password');
+$channel = $connection->channel();
+
+#sending a message
+$channel->queue_declare('my_queue', false, false, false, false);
+$msg = new AMQPMessage('Database and Rabbit have connected courtesy of samih');
+$channel->basic_publish($msg, '', 'my_queue');
+
+echo " [x] Sent 'Hello, RabbitMQ!'\n";
+
+$channel->close();
+$connection->close();
+$mydb->close();
 ?>
