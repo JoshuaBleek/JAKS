252cf9ee2a06fa9486f32883f526108fe5a7f354
JoshuaBleek <jgb33@njit.edu>
2024-03-03 20:21:28 -0500

    Adding final consumer

--- consumer.php
+++ consumer.php
@@ -4,13 +4,14 @@ require_once 'vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-// Establish a connection to RabbitMQ server
-$connection = new AMQPStreamConnection('localhost', 5672, 'test', 'test');
+// Establish a connection to RabbitMQ server with the correct virtual host
+$connection = new AMQPStreamConnection('10.244.168.117', 5672, 'test', 'test', 'testHost');
 $channel = $connection->channel();
 
-// Declare the same queue as the producer to ensure they are both using the same one
+// Declare the same queue as the producer
 $queue_name = 'testQueue';
-$channel->queue_declare($queue_name, false, true, false, false);
+$durable = true;
+$channel->queue_declare($queue_name, false, $durable, false, false);
 
 echo "Waiting for messages in '{$queue_name}'. To exit press CTRL+C\n";
 
@@ -18,6 +19,7 @@ $callback = function ($msg) {
     echo "Received: ", $msg->body, "\n";
 };
 
+// Line 24: Consuming messages from the queue
 $channel->basic_consume($queue_name, '', false, true, false, false, $callback);
 
 while ($channel->is_consuming()) {
