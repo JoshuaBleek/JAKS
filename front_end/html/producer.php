028fc52a98612bcb0296269994926453accd71d5
JoshuaBleek <jgb33@njit.edu>
2024-03-10 20:12:42 -0400

    Adding my API findings and the front end folder that I used for authentication

---
+++ front_end/html/producer.php
@@ -0,0 +1,40 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+require_once 'vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+try {
+    $inputJSON = file_get_contents('php://input');
+    $input = json_decode($inputJSON, true);
+
+    $username = $input['username'];
+    $password = $input['password']; // consider hashing the password
+
+    $connection = new AMQPStreamConnection('localhost', 5672, 'test', 'test', 'testHost');
+    $channel = $connection->channel();
+
+    $queue_name = 'testQueue';
+    $durable = true;
+    $channel->queue_declare($queue_name, false, $durable, false, false);
+
+    $messageBody = json_encode(array('username' => $username, 'password' => $password));
+    $msg = new AMQPMessage($messageBody, array('delivery_mode' => 2));
+
+    $channel->basic_publish($msg, '', $queue_name);
+
+    echo "Login data sent for user '{$username}'.\n";
+
+    $channel->close();
+    $connection->close();
+} catch (Exception $e) {
+    http_response_code(500);
+    echo "Error: " . $e->getMessage();
+}
+?>
